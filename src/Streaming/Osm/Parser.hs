1dd17753bb9bf2ad715ca4d4367faf4c59f32362
Colin Woodbury <colingw@gmail.com>
2017-04-24 11:34:33 -0700

    Added many parsers
    - The Element types are in flux. The underlying protobuf is making
      certain demands on numeric types that I am dealing with loosly.

--- src/Streaming/Osm/Parser.hs
+++ src/Streaming/Osm/Parser.hs
@@ -10,6 +10,9 @@ import           Data.Bits
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import           Data.Int
+import qualified Data.Map as M
+import qualified Data.Vector as V
+import           Data.Word
 import           Streaming.Osm.Types
 import           Streaming.Osm.Util
 
@@ -36,7 +39,7 @@ headerBlock :: A.Parser [B.ByteString]
 headerBlock = do
   optional (A.word8 0x0a *> varint >>= A.take)
   res <- A.many' (A.word8 0x22 *> varint >>= A.take)
-  A.many' (A.word8 0x2a *> varint >>= A.take)                   -- optional_features
+  A.many'  (A.word8 0x2a *> varint >>= A.take)                  -- optional_features
   optional (A.word8 0x82 *> A.word8 0x01 *> varint >>= A.take)  -- writingprogram
   optional (A.word8 0x8a *> A.word8 0x01 *> varint >>= A.take)  -- source
   optional (A.word8 0x80 *> A.word8 0x02 *> varint @Int64)      -- osmosis_replication_timestamp
@@ -44,14 +47,82 @@ headerBlock = do
   optional (A.word8 0x92 *> A.word8 0x02 *> varint >>= A.take)  -- osmosis_replication_bas_url
   pure res
 
+-- TODO: many or many' ?
+
+-- TODO: Handle DenseNodes
+-- | Called a @PrimitiveBlock@ in the OSM literature.
+block :: A.Parser Block
+block = do
+  st <- A.word8 0x0a *> varint @Int *> stringTable
+  ns <- A.word8 0x12 *> varint @Int *> A.many' (node st)
+  ws <- A.word8 0x12 *> varint @Int >>= A.take >> pure [] -- *> A.many' (way st)
+  rs <- A.word8 0x12 *> varint @Int >>= A.take >> pure [] -- *> A.many' (relation st)
+  gran <- (A.word8 0x88 *> A.word8 0x01 *> varint @Int64) <|> pure 100   -- granularity
+  date <- (A.word8 0x90 *> A.word8 0x01 *> varint @Int32) <|> pure 1000  -- date_granularity
+  lato <- (A.word8 0x98 *> A.word8 0x01 *> varint @Int64) <|> pure 0     -- lat_offset
+  lono <- (A.word8 0xa0 *> A.word8 0x01 *> varint @Int64) <|> pure 0     -- lon_offset
+  pure $ Block (map (\f -> f gran lato lono) ns) ws rs
+
+-- | The String Table will never be empty, since all Elements have
+-- non-geographic metadata (username, etc.) which contain Strings. The result
+-- must be a `V.Vector`, since we need random access to its contents.
+stringTable :: A.Parser (V.Vector B.ByteString)
+stringTable = V.fromList <$> A.many1 (A.word8 0x0a *> varint >>= A.take)
+
+-- | Parse a `Node`. Uses `V.unsafeIndex` to quickly retrieve its tag
+-- Strings, assuming that the Node's key/value pairs will always index a legal
+-- value in the given String Table.
+node :: V.Vector B.ByteString -> A.Parser (Int64 -> Int64 -> Int64 -> Node)
+node st = do
+  A.word8 0x0a
+  varint @Int
+  i   <- unzig <$> (A.word8 0x08 *> varint @Word64)
+  ks  <- (A.word8 0x12 *> varint @Int *> A.many1 varint) <|> pure []
+  vs  <- (A.word8 0x1a *> varint @Int *> A.many1 varint) <|> pure []
+  oi  <- optional (A.word8 0x22 *> infoP i st)
+  lat <- unzig <$> (A.word8 0x40 *> varint @Word64)
+  lon <- unzig <$> (A.word8 0x48 *> varint @Word64)
+  let ts = M.fromList $ zip (map (V.unsafeIndex st) ks) (map (V.unsafeIndex st) vs)
+  pure $ (\gran lato lono -> Node (offset lato gran lat) (offset lono gran lon) oi ts)
+
+way :: V.Vector B.ByteString -> A.Parser Way
+way st = undefined
+
+relation :: V.Vector B.ByteString -> A.Parser Relation
+relation st = undefined
+
+infoP :: Int64 -> V.Vector B.ByteString -> A.Parser Info
+infoP i st = Info
+  <$> pure (fromIntegral i)
+  <*> ((A.word8 0x08 *> varint) <|> pure (-1))
+  <*> optional (A.word8 0x10 *> varint)
+  <*> optional (A.word8 0x18 *> varint)
+  <*> optional (A.word8 0x20 *> varint)
+  <*> optional (V.unsafeIndex st <$> (A.word8 0x28 *> varint))
+  <*> ((>>= booly) <$> optional (A.word8 0x30 *> varint @Word8))
+
 -- | Parse some Varint, which may be made up of multiple bytes.
 varint :: (Num a, Bits a) => A.Parser a
 varint = foldBytes' <$> A.takeWhile (\b -> testBit b 7) <*> A.anyWord8
 {-# INLINABLE varint #-}
 
-test :: IO (Either String [B.ByteString])
+-- | Restore truncated LatLng values to their true `Double` representation.
+offset :: Int64 -> Int64 -> Int64 -> Double
+offset off gran coord = 0.000000001 * fromIntegral (off + (gran * coord))
+
+-- | Decode a Z-encoded Word64 into a 64-bit Int.
+unzig :: Word64 -> Int64
+unzig n = fromIntegral unzigged
+  where unzigged = shift n (-1) `xor` negate (n .&. 1)
+
+booly :: Word8 -> Maybe Bool
+booly 0 = Just False
+booly 1 = Just True
+booly _ = Nothing
+
+--test :: IO (Either String [B.ByteString])
 test = do
   bytes <- B.readFile "diomede.osm.pbf"
-  pure $ A.parseOnly ((,) <$> header <*> blob) bytes >>= f
-    where f (_, Blob { bytes = Left bs }) = A.parseOnly headerBlock bs
-          f (_, Blob { bytes = Right (_, bs) }) = A.parseOnly headerBlock . BL.toStrict . decompress $ BL.fromStrict bs
+  pure $ A.parseOnly (header *> blob *> header *> blob) bytes >>= f
+    where f (Blob { bytes = Left bs }) = A.parseOnly block bs
+          f (Blob { bytes = Right (_, bs) }) = A.parseOnly block . BL.toStrict . decompress $ BL.fromStrict bs
--- src/Streaming/Osm/Types.hs
+++ src/Streaming/Osm/Types.hs
@@ -1,13 +1,14 @@
 module Streaming.Osm.Types
   ( -- * Elements
-    Element(..)
-  , Node(..)
+--    Element(..)
+    Node(..)
   , Way(..)
   , Relation(..)
   , Info(..)
   -- * Helper Types
   , BlobHeader(..)
   , Blob(..)
+  , Block(..)
   ) where
 
 import qualified Data.ByteString as B
@@ -17,38 +18,43 @@ import           Data.Text (Text)
 
 ---
 
+{-}
 class Element a where
   info :: a -> Info
   tags :: a -> M.Map Text Text
+-}
 
 data Node = Node { _lat   :: Double
                  , _lng   :: Double
-                 , _ninfo :: Info
-                 , _ntags :: M.Map Text Text
+                 , _ninfo :: Maybe Info
+                 , _ntags :: M.Map B.ByteString B.ByteString
                  } deriving (Eq, Show)
 
+{-}
 instance Element Node where
   info = _ninfo
   tags = _ntags
-
+-}
 data Way = Way { _nodeRefs :: [Int]
                , _winfo    :: Info
                , _wtags    :: M.Map Text Text
                } deriving (Eq, Show)
 
+{-}
 instance Element Way where
   info = _winfo
   tags = _wtags
-
+-}
 data Relation = Relation { _members :: [Member]
                          , _rinfo   :: Info
                          , _rtags   :: M.Map Text Text
                          } deriving (Eq, Show)
 
+{-}
 instance Element Relation where
   info = _rinfo
   tags = _rtags
-
+-}
 data Member = Member { _mref :: Int, _mrole :: Text } deriving (Eq, Show)
 
 -- | Non-geographic `Element` metadata. The OSM database is a wild place, so
@@ -58,7 +64,7 @@ data Info = Info { _id        :: Int
                  , _timestamp :: Maybe Int
                  , _changeset :: Maybe Int
                  , _uid       :: Maybe Int
-                 , _username  :: Maybe Text
+                 , _username  :: Maybe B.ByteString
                  , _visible   :: Maybe Bool
                  } deriving (Eq, Show)
 
@@ -66,4 +72,9 @@ data BlobHeader = BlobHeader { blobType  :: B.ByteString
                              , indexData :: Maybe B.ByteString
                              , datasize  :: Int32 } deriving (Show)
 
-data Blob = Blob { bytes :: Either B.ByteString (Int32, B.ByteString) } deriving (Show)
+data Blob = Blob { bytes :: Either B.ByteString (Int32, B.ByteString) } deriving (Eq, Show)
+
+-- | A group of ~8000 OSM Elements.
+data Block = Block { nodes :: [Node]
+                   , ways  :: [Way]
+                   , relations :: [Relation] } deriving (Eq, Show)
