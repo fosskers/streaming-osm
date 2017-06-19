5431488e74a027598457435f65cc1cf4f610e424
Colin Woodbury <colingw@gmail.com>
2017-06-19 14:14:59 -0700

    (opt) Assume the offset values

--- src/Streaming/Osm/Parser.hs
+++ src/Streaming/Osm/Parser.hs
@@ -46,13 +46,11 @@ block = do
   dn <- (A.word8 0x12 *> varint @Int *> dense st) <|> pure []
   ws <- (A.word8 0x12 *> varint @Int *> A.many1' (way st)) <|> pure []
   rs <- (A.word8 0x12 *> varint @Int *> A.many1' (relation st)) <|> pure []
-  gran <- (A.word8 0x88 *> A.word8 0x01 *> varint @Int64) <|> pure 100   -- granularity
-  date <- (A.word8 0x90 *> A.word8 0x01 *> varint @Int32) <|> pure 1000  -- date_granularity
-  lato <- (A.word8 0x98 *> A.word8 0x01 *> varint @Int64) <|> pure 0     -- lat_offset
-  lono <- (A.word8 0xa0 *> A.word8 0x01 *> varint @Int64) <|> pure 0     -- lon_offset
-  pure $ Block (map (\f -> f gran date lato lono) $ ns ++ dn)
-               (map (\f -> f date) ws)
-               (map (\f -> f date) rs)
+  optional (A.word8 0x88 *> A.word8 0x01 *> varint @Int64)  -- granularity
+  optional (A.word8 0x90 *> A.word8 0x01 *> varint @Int32)  -- date_granularity
+  optional (A.word8 0x98 *> A.word8 0x01 *> varint @Int64)  -- lat_offset
+  optional (A.word8 0xa0 *> A.word8 0x01 *> varint @Int64)  -- lon_offset
+  pure $ Block (ns ++ dn) ws rs
 
 -- | The String Table will never be empty, since all Elements have
 -- non-geographic metadata (username, etc.) which contain Strings. The result
@@ -63,7 +61,7 @@ stringTable = V.fromList <$> A.many1' (A.word8 0x0a *> varint >>= A.take)
 -- | Parse a `Node`. Uses `V.unsafeIndex` to quickly retrieve its tag
 -- Strings, assuming that the Node's key/value pairs will always index a legal
 -- value in the given String Table.
-node :: V.Vector B.ByteString -> A.Parser (Int64 -> Int32 -> Int64 -> Int64 -> Node)
+node :: V.Vector B.ByteString -> A.Parser Node
 node st = do
   A.word8 0x0a *> varint @Int
   i   <- unzig <$> (A.word8 0x08 *> varint)                          -- id
@@ -73,19 +71,19 @@ node st = do
   lat <- unzig <$> (A.word8 0x40 *> varint)                          -- lat
   lon <- unzig <$> (A.word8 0x48 *> varint)                          -- lon
   let ts = M.fromList $ zip (map (V.unsafeIndex st) ks) (map (V.unsafeIndex st) vs)
-  pure $ (\gran dgran lato lono -> Node (offset lato gran lat) (offset lono gran lon) (($ dgran) <$> oi) ts)
+  pure $ Node (offset lat) (offset lon) oi ts
 
 -- | Parse a @DenseNodes@ in a similar way to `node`.
-dense :: V.Vector B.ByteString -> A.Parser [Int64 -> Int32 -> Int64 -> Int64 -> Node]
+dense :: V.Vector B.ByteString -> A.Parser [Node]
 dense st = do
   A.word8 0x12 *> varint @Int
   ids <- undelta . map unzig . packed <$> (A.word8 0x0a *> varint >>= A.take)
-  ifs <- (A.word8 0x2a *> varint @Int *> denseInfo ids st) <|> pure (repeat $ const Nothing)
+  ifs <- (A.word8 0x2a *> varint @Int *> denseInfo ids st) <|> pure (repeat Nothing)
   lts <- undelta . map unzig . packed <$> (A.word8 0x42 *> varint >>= A.take)
   lns <- undelta . map unzig . packed <$> (A.word8 0x4a *> varint >>= A.take)
   kvs <- (packed <$> (A.word8 0x52 *> varint >>= A.take)) <|> pure []
   pure $ zipWith4 f lts lns ifs (denseTags st kvs)
-  where f lat lon inf ts = \gran dgran lato lono -> Node (offset lato gran lat) (offset lono gran lon) (inf dgran) ts
+  where f lat lon inf ts = Node (offset lat) (offset lon) inf ts
 
 -- | Interpret a list of flattened key-value pairs as Tag metadata `Map`s.
 denseTags :: V.Vector B.ByteString -> [Int] -> [M.Map B.ByteString B.ByteString]
@@ -98,7 +96,7 @@ packed bs = {-# SCC packed #-} either (const []) id $ A.parseOnly (A.many1' vari
 {-# INLINE packed #-}
 
 -- | Parse a `Way`.
-way :: V.Vector B.ByteString -> A.Parser (Int32 -> Way)
+way :: V.Vector B.ByteString -> A.Parser Way
 way st = do
   A.word8 0x1a *> varint @Int
   i <- A.word8 0x08 *> varint                                       -- id
@@ -107,13 +105,13 @@ way st = do
   oi <- optional (A.word8 0x22 *> varint @Int *> info i st)         -- info
   ns <- undelta . map unzig . packed <$> (A.word8 0x42 *> varint >>= A.take)
   let ts = M.fromList $ zip (map (V.unsafeIndex st) ks) (map (V.unsafeIndex st) vs)
-  pure $ (\dgran -> Way ns (($ dgran) <$> oi) ts)
+  pure $ Way ns oi ts
 
 -- | Parse a `Relation`.
-relation :: V.Vector B.ByteString -> A.Parser (Int32 -> Relation)
+relation :: V.Vector B.ByteString -> A.Parser Relation
 relation st = do
   A.word8 0x22 *> varint @Int
-  i <- A.word8 0x08 *> varint
+  i  <- A.word8 0x08 *> varint
   ks <- packed <$> (A.word8 0x12 *> varint >>= A.take) <|> pure []                -- keys
   vs <- packed <$> (A.word8 0x1a *> varint >>= A.take) <|> pure []                -- vals
   oi <- optional (A.word8 0x22 *> varint @Int *> info i st)                       -- info
@@ -122,9 +120,9 @@ relation st = do
   ts <- map memtype . packed <$> (A.word8 0x52 *> varint >>= A.take) <|> pure []  -- types
   let tags = M.fromList $ zip (map (V.unsafeIndex st) ks) (map (V.unsafeIndex st) vs)
       mems = zipWith3 Member ms ts $ map (V.unsafeIndex st) rs
-  pure $ (\dgran -> Relation mems (($ dgran) <$> oi) tags)
+  pure $ Relation mems oi tags
 
-info :: Int64 -> V.Vector B.ByteString -> A.Parser (Int32 -> Info)
+info :: Int64 -> V.Vector B.ByteString -> A.Parser Info
 info i st = do
   vn <- ((A.word8 0x08 *> varint) <|> pure (-1))                    -- version
   ts <- optional (A.word8 0x10 *> varint)                           -- timestamp
@@ -132,10 +130,10 @@ info i st = do
   ui <- optional (A.word8 0x20 *> varint)                           -- uid
   us <- optional (V.unsafeIndex st <$> (A.word8 0x28 *> varint))    -- user_sid
   vi <- ((>>= booly) <$> optional (A.word8 0x30 *> varint @Word8))  -- visible
-  pure $ (\dgran -> Info (fromIntegral i) vn (toffset dgran <$> ts) cs ui us vi)
+  pure $ Info (fromIntegral i) vn (toffset <$> ts) cs ui us vi
 
 -- | Parse a @DenseInfo@ message.
-denseInfo :: [Int] -> V.Vector B.ByteString -> A.Parser [Int32 -> Maybe Info]
+denseInfo :: [Int] -> V.Vector B.ByteString -> A.Parser [Maybe Info]
 denseInfo nis st = do
   ver <- packed <$> (A.word8 0x0a *> varint >>= A.take)
   tms <- map Just . undelta . map unzig . packed <$> (A.word8 0x12 *> varint >>= A.take)
@@ -143,9 +141,8 @@ denseInfo nis st = do
   uid <- map Just . undelta . map unzig . packed <$> (A.word8 0x22 *> varint >>= A.take)
   uss <- map (st V.!?) . undelta . map unzig . packed <$> (A.word8 0x2a *> varint >>= A.take)
   vis <- (map booly . packed <$> (A.word8 0x32 *> varint >>= A.take)) <|> pure (repeat $ Just True)
-  pure . map f $ zipWith7 Info nis ver tms chs uid uss vis
-    where f i@(Info _ _ (Just ts) _ _ _ _) dgran = Just $ i { _timestamp = Just $ toffset dgran ts }
-          f i _ = Just i
+  pure $ zipWith7 f nis ver tms chs uid uss vis
+    where f ni vs tm ch ui us vi = Just $ Info ni vs (toffset <$> tm) ch ui us vi
 
 -- | Parse some Varint, which may be made up of multiple bytes.
 varint :: (Num a, Bits a) => A.Parser a
@@ -153,13 +150,14 @@ varint = {-# SCC varint #-} foldBytes' <$> A.takeWhile (\b -> testBit b 7) <*> A
 {-# INLINE varint #-}
 
 -- | Restore truncated LatLng values to their true `Double` representation.
-offset :: Int64 -> Int64 -> Int64 -> Double
-offset off gran coord = {-# SCC offset #-} 0.000000001 * fromIntegral (off + (gran * coord))
+offset :: Int64 -> Double
+offset coord = {-# SCC offset #-} 0.000000001 * fromIntegral (100 * coord)
 {-# INLINE offset #-}
 
 -- | Restore truncated timestamps to the number of millis since the 1970 epoch.
-toffset :: Int32 -> Int64 -> Int64
-toffset gran time = fromIntegral gran * time
+toffset :: Int64 -> Int64
+toffset time = 1000 * time
+{-# INLINE toffset #-}
 
 -- TODO: Is this right?
 booly :: Word8 -> Maybe Bool
